b93e1b9195bc8c1ef93acc3884deb483499c34a0
Hemant Achhami <hemantachhami19@gmail.com>
2018-10-28 05:24:14 +0545

    Mocha test with chai

--- utils/util.test.js
+++ utils/util.test.js
@@ -12,3 +12,13 @@ it('should square the  number 6',()=>{
    assert.equal(result,36,"Two values are not equal")
 });
 
+describe('Basic Mocha String Test', function () {
+    it('should return number of charachters in a string', function () {
+        assert.equal("Hello".length, 5);
+    });
+    it('should return first charachter of the string', function () {
+        assert.equal("Hello".charAt(0), 'H');
+        //throw {myError:'throwing error to fail test'}
+    });
+});
+
