54b0178d14d56becf756bf50286f8cb152b9aa8f
Hemant Achhami <hemantachhami19@gmail.com>
2018-10-28 05:17:43 +0545

    Testing  node app with mocha and chai

--- utils/util.test.js
+++ utils/util.test.js
@@ -1,17 +1,14 @@
 //Mocha goes through the project
 const utils = require('./utils');
+var assert = require('assert');
+
 it('should add two numbers',()=>{
   var res = utils.add(2,33);
-  if(res !== 35){
-      console.log(res);
-      throw new Error(`Expected 35 but got ${res}`);
-  }
+  assert.equal(res, 35, 'test passes');
 });
 
 it('should square the  number 6',()=>{
    var result = utils.square(6);
-   if(result !== 36){
-       throw  new Error(`Exected 36 got ${result}`);
-   }
+   assert.equal(result,36,"Two values are not equal")
 });
 
